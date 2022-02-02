6b592aeb9c4284660eb906e455d2615fb6e82dbb
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2022-02-02 18:57:35 -0300

    initialize

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import getUser from '../utils/getUsers'
+
+const Index = ({ repos, user }) => {
+  return(
+    <div className='container mx-auto'>
+      <h1 className='text-5xl'>Olá, eu sou o Vinícius </h1>
+      <h2 className='font-bold text-3xl'>Meus repositórios no Github</h2>
+      <p>Github stats: public repos:{user.public_repos} / public gists: {user.public_gists} / followers: {user.followers}</p>
+        {repos.map(repo => {
+          return (
+            <div key={repo.id} className='rounded bg-gray-200 mx-8 my-4 p-4 hover:shadow-md'>
+              <h3 className='font-bold'>{repo.full_name}</h3>
+              <p>Language: {repo.language} / Stars: {repo.stargazers_count}</p>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+
+  const { repos, user } = await getUser('viniciusmarquezaninelo')
+  return {
+    props: {
+      currentDate: new Date().toString(),
+      repos,
+      user
+    }
+  }
+}
+export default Index
