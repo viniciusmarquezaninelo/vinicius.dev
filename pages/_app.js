f4aa0c86f756a2829dfdbd1b7a08666e48203fca
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2022-02-07 21:29:10 -0300

    Adding custom fonts In

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,19 @@ import React from 'react'
 import App from 'next/app'
 import '../css/styles.css'
 
+
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
-    return <Component {...pageProps} />
+    return (
+      <>
+      <style jsx global> {`
+        @import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;700&display=swap');
+        `}
+      </style>  
+        <Component {...pageProps} />
+      </>
+    )
   }
 }
 
