e7065c7fc7bd2a75f324e5f2dbd5a7ffbb476481
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2022-02-15 21:41:42 -0300

    adding styles

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,21 @@ import getUser from '../utils/getUser'
 const Index = ({ repos, user }) => {
   return(
     <div className='container mx-auto'>
-      <h1 className='text-5xl'>Olá, eu sou o Vinícius </h1>
-      <h2 className='font-bold text-3xl'>Meus repositórios no Github</h2>
-      <p>Github stats: public repos:{user.public_repos} / public gists: {user.public_gists} / followers: {user.followers}</p>
+      <div className='grid grid-cols-2 pt-16'>
+        <div>
+          <h1 className='text-4xl uppercase '>Hi, I'm Vinicius</h1>
+          <h2 className='font-bold text-5xl uppercase'>Fullstack Developer</h2>
+          <h3>Contact</h3>
+          <ul>
+            <li>linkedin</li>
+            <li>Github</li>
+          </ul>
+          <p>Github stats: public repos:{user.public_repos} / public gists: {user.public_gists} / followers: {user.followers}</p>
+        </div>
+        <div>
+          <img src="/images/pngs/vinicius-foto.png" />
+        </div>
+      </div>
         {repos.map(repo => {
           return (
             <div key={repo.id} className='rounded bg-gray-200 mx-8 my-4 p-4 hover:shadow-md'>
