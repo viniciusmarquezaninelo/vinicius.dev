7e8df79f1138c8cad88f10c1a99ec00ac79335c0
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2022-03-20 19:43:02 -0300

    Changing page title and favicon

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Head from 'next/head'
 import getUser from '../utils/getUser'
 
 import { AiFillLinkedin, AiOutlineGithub, AiOutlineMail, AiOutlineWhatsApp } from "react-icons/ai";
@@ -10,6 +10,26 @@ import { GoStar } from 'react-icons/go';
 const Index = ({ repos, user }) => {
   return(
     <div className='container mx-auto bg-network'>
+      <Head>
+        <title>Vinícius Zaninelo - Fullstack Developer</title>
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff"/>
+      </Head>
       <div className='grid grid-cols-2 pt-16 leading-none'>
         <div className="pt-32">
           <h1 className='text-4xl uppercase pl-16'>Hi, I'm Vinicius</h1>
@@ -65,6 +85,9 @@ const Index = ({ repos, user }) => {
           })}
         </div>
       </div>
+        <div>
+          <p className="text-center my-8 py-4 border-t-2">You can find the source-code of this website here.</p>
+        </div>
     </div>
   );
 }
