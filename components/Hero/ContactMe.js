6036aed08d1d5005dedd4b0a54a06717e4467ab9
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2022-05-28 12:21:46 -0300

    putting contact information

--- components/Hero/ContactMe.js
+++ components/Hero/ContactMe.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 import { AiFillLinkedin, AiOutlineGithub, AiOutlineMail, AiOutlineWhatsApp } from "react-icons/ai";
 
+
 const ContactMe = () =>  {
   return(
     <div className="mx-8 px-8 relative border border-lightPink rounded md:mx-0 md:px-16 pb-4 pt-10 mt-6">
       <h3 className="absolute bg-lightPink text-white py-2 px-6 top-0 -mt-5 text-2xl font-bold uppercase">Contact me</h3>
       <p className="text-center md:text-left">
-        <AiFillLinkedin className='text-6xl inline-block mr-6'/> 
-        <AiOutlineGithub className='text-6xl inline-block mr-6'/>
-        <AiOutlineMail className='text-6xl inline-block mr-6'/>
-        <AiOutlineWhatsApp className='text-6xl inline-block mr-6'/>
+        <a href='https://www.linkedin.com/in/vin%C3%ADciuszaninelo/'><AiFillLinkedin className='text-6xl inline-block mr-6'/></a>
+        <a href='https://github.com/viniciusmarquezaninelo'><AiOutlineGithub className='text-6xl inline-block mr-6'/></a>
+        <a href='mailto:vinicousmarquezaninelo@gmail.com?'><AiOutlineMail className='text-6xl inline-block mr-6'/></a>
+        <a href='https://web.whatsapp.com/send?phone=554488314504'><AiOutlineWhatsApp className='text-6xl inline-block mr-6'/></a>
       </p>
     </div>
   );
