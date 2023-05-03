8d19aa0c8479ba8304e330064e24c01cab1adab0
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2023-05-02 22:04:26 -0300

    :recycle:

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,12 +7,14 @@ export function Experience() {
       <h5>My Recent Work</h5>
       <h2>My Experiences</h2>
 
-      <div className="container items-center flex justify-center">
-        <div className="w-1  h-96 bg-linhatempo relative"></div>{/* LINHA DO TEMPO */}
+      <div className="container items-center flex justify-center"> {/* LINHA DO TEMPO */}
+        <div className="w-1  h-96 bg-linhatempo">
+            <div className="w-20 h-20 bg-circles rounded-full">{/* CIRCULO COM LOGO DA EMPRESA */}
+              <img src={LogoIw} alt="logo iw" />
+            </div> 
+          </div>
     
-        <div className="w-20 h-20 bg-circles rounded-full absolute">{/* CIRCULO COM LOGO DA EMPRESA */}
-          <img src={LogoIw} alt="logo iw" />
-        </div> 
+        
       </div>
     </section>
   )
